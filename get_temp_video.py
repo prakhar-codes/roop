d77b197759a59d09bcb468137bd1ef1aefdc02cb
Prakhar Saxena <saxenaprakhar2004@gmail.com>
2023-11-05 13:32:34 +0530

    modified get_temp_video.py

--- get_temp_video.py
+++ get_temp_video.py
@@ -14,26 +14,24 @@ frame_size = (1280, 720)  # Set the frame size to match your images
 # Get the list of frame files sorted by frame number
 frame_files = sorted([f for f in os.listdir(frame_folder) if f.endswith(".png")], key=lambda x: int(os.path.splitext(x)[0]))
 
-temp_length = len(frame_files)
-# temp_length = custom length
+target_frame = len(frame_files)
+# target_frame = x (frame to target)
+target_filename = f"{target_frame:04}.png"
 
 # Create a VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 out = cv2.VideoWriter(output_video_path, fourcc, frame_rate, frame_size)
 
 # Create a tqdm progress bar to track the processing
-progress_bar = tqdm(total=len(frame_files), unit="frame")
-
-counter = 0
+progress_bar = tqdm(total=target_frame, unit="frame")
 
 # Loop through the frame images and write them to the video
 for frame_file in frame_files:
-    counter=counter+1
     frame_path = os.path.join(frame_folder, frame_file)
     frame = cv2.imread(frame_path)
     out.write(frame)
     progress_bar.update(1)  # Update the progress bar
-    if(counter == temp_length) : break
+    if(frame_file == target_filename) : break
 
 # Release the VideoWriter object
 out.release()
